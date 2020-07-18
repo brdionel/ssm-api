829bdf96a2faaa5363857a61db6949af6e23e026
Franco Gomez <franco.gomez@thefuzzyfish.com>
2020-07-18 20:50:58 -0300

    added CORS headers

--- server.js
+++ server.js
@@ -9,5 +9,14 @@ app.listen(8000, () => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 const router = require('./api/router/router');
 router(app);
